4f174bd07617af9b80f3a0541f38198dcf1543fc
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-04-27 17:30:36 +0200

    Update courses.ts

--- data/courses.ts
+++ data/courses.ts
@@ -1,1 +1,91 @@
+export const data = {
+  Mathématiques: {
+    College: {
+      "6e": ["Fractions", "Nombres décimaux", "Proportionnalité", "Géométrie"],
+      "5e": ["Fractions avancées", "Pourcentages", "Volumes", "Symétrie"],
+      "4e": ["Pythagore", "Puissances", "Equations", "Statistiques"],
+      "3e": ["Fonctions", "Trigonométrie", "Probabilités", "Suites simples"],
+    },
 
+    Lycee: {
+      "2nde": ["Fonctions", "Vecteurs", "Probabilités", "Statistiques"],
+      "1ere": ["Dérivation", "Suites", "Exponentielle", "Trigonométrie"],
+      "Terminale": ["Limites", "Intégrales", "Suites avancées", "Probabilités"],
+    },
+
+    Prepa: {
+      MPSI: [
+        "Algèbre linéaire",
+        "Suites et séries",
+        "Analyse réelle",
+        "Fonctions de plusieurs variables"
+      ],
+      PCSI: [
+        "Espaces vectoriels",
+        "Séries",
+        "Calcul différentiel",
+        "Analyse complexe"
+      ],
+    },
+
+    GrandesEcoles: {
+      HEC: [
+        "Probabilités avancées",
+        "Statistiques",
+        "Optimisation",
+        "Analyse de données"
+      ],
+      "Ingénieur": [
+        "Algèbre avancée",
+        "Analyse numérique",
+        "Calcul scientifique"
+      ]
+    }
+  },
+
+  Physique: {
+    College: {
+      "6e": ["Matière", "Energie"],
+      "5e": ["Forces", "Mouvement"],
+      "4e": ["Electricité", "Vitesse"],
+      "3e": ["Ondes", "Optique"]
+    },
+
+    Lycee: {
+      "2nde": ["Mouvement", "Forces", "Energie"],
+      "1ere": ["Ondes", "Electricité"],
+      "Terminale": ["Mécanique", "Optique", "Thermodynamique"]
+    },
+
+    Prepa: {
+      MPSI: ["Mécanique du point", "Thermodynamique", "Ondes"],
+      PCSI: ["Mécanique avancée", "Electromagnétisme", "Chimie physique"]
+    },
+
+    GrandesEcoles: {
+      "Ingénieur": ["Mécanique avancée", "Physique quantique", "Optique avancée"]
+    }
+  },
+
+  Chimie: {
+    College: {
+      "5e": ["Mélanges", "Transformations chimiques"],
+      "4e": ["Réactions chimiques"],
+      "3e": ["Acides et bases"]
+    },
+
+    Lycee: {
+      "2nde": ["Structure de la matière"],
+      "1ere": ["Réactions chimiques", "Energie"],
+      "Terminale": ["Cinétique chimique", "Equilibres"]
+    },
+
+    Prepa: {
+      PCSI: ["Thermodynamique chimique", "Cinétique", "Equilibres"]
+    },
+
+    GrandesEcoles: {
+      "Ingénieur": ["Chimie organique", "Chimie industrielle"]
+    }
+  }
+};
